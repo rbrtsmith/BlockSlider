a2755ef56fa4bf2eb1037623089922061d7ac390
Robert Smith <robert@wasabiweb.se>
2015-10-01 14:58:21 +0200

    added dist minified version

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,20 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     esLint = require('gulp-eslint'),
     plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
     babel = require('gulp-babel');
 
 
 gulp.task('JS', function() {
-
+	return gulp
+		.src('src/block-slider.js')
+		.pipe(plumber())
+		.pipe(esLint())
+		.pipe(esLint.format())
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist'))
 });
+
+gulp.task('default', ['JS']);
--- src/block-slider.js
+++ src/block-slider.js
@@ -10,6 +10,22 @@ function BlockSlider(collection, options) {
     if (!collection.length && !collection.querySelector) {
         return;
     }
+
+    function debounce (func, wait, immediate) {
+        let timeout;
+        return () => {
+            const context = this, 
+                args = arguments;
+            const later = () => {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            const callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    }
     
     options = options || {};
     options.slideInterval = options.slideInterval || 500;
@@ -95,7 +111,7 @@ function BlockSlider(collection, options) {
     setWidths();
 
     window.addEventListener('resize', () => {
-        window.requestAnimationFrame(WW.debounce(() => {
+        window.requestAnimationFrame(debounce(() => {
             setWidths();
             slide();
         }, 300));
