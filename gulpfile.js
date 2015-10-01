742beec9bf3e238cea0e4dc253499ec411c6b35d
Robert Smith <robert@wasabiweb.se>
2015-10-01 14:49:21 +0200

    -

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    esLint = require('gulp-eslint'),
+    plumber = require('gulp-plumber'),
+    babel = require('gulp-babel');
+
+
+gulp.task('JS', function() {
+
+});
